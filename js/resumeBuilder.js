6ba6bec308acae043ac484b1b693d9855d5fa104
wangela <wangela@users.noreply.github.com>
2016-12-13 01:15:10 -0800

    Sync after Array Manipulation and String Manipulation lessons

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,18 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Angela Yu";
+var role = "IoT Engineering Leader";
+
+var awesomeThoughts = "I am Angela and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+var myArray = ["zero", "one", "two"]
+
+console.log(myArray.length);
+console.log(awesomeThoughts);
+console.log(funThoughts);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#main").append(funThoughts);
